a1e70982a25b6ce006f491094ef6dd8a44aae34a
Kaan Şengün <427642@ogr.ktu.edu.tr>
2023-11-13 19:30:47 +0300

    Last Situation

--- classConsept/main.cpp
+++ classConsept/main.cpp
@@ -6,9 +6,35 @@
 //
 
 #include <iostream>
+#include "studentClass.h"
+using namespace std;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    //Sınıf tanımında hafızadan yer ayrılmaz ama nesne üretildiğinde yer kaplar(Kapladığı alan da nesnenin verileri kadar)
+    Student student1("Ogun","Birinci");
+    Student student2("Duygu","Keydal","4157");
+    Student student3(student1);
+    student3.displayInformation();
+    
+    Student studentArray[5];
+    
+    /*student1.setName("Ogun");
+    student1.setSurname("Birinci");
+    student1.setID("3000");
+    */
+    string name=student1.getName();
+    string surname=student1.getSurname();
+    string ID=student1.getID();
+    
+    cout<<name<<endl;
+    cout<<surname<<endl;
+    cout<<ID<<endl;
+    
+    student2.displayInformation();
+    
+    if(true){
+        Student student5;
+    }//Local bir nesne oluşturduk
+    cout<<"student5"<<endl;
     return 0;
 }
--- classConsept/studentClass.h
+++ classConsept/studentClass.h
@@ -5,8 +5,71 @@
 //  Created by Kaan Şengün on 13.11.2023.
 //
 
-#ifndef studentClass_h
-#define studentClass_h
+#include <iostream>
+using namespace std;
+/**
+ ERİŞİM BELİRLEYİCİLERİ
+ 
+ private---->Sadece sınıfın içerisinden erişilebilir.(Default olarak private kullanılır)
+ protected-->Sadece class içerisinde ve miras alan classlarda erişilebilir main fonksiyonundan erişilemez.
+ public----->Her yerden erişilebilir.
+ 
+ */
 
-
-#endif /* studentClass_h */
+class Student{
+    string studentName,studentSurname,studentID;//üye değişkenler(member variable)
+public:
+    Student(){
+        cout<<"Default constructor worked."<<endl;
+        studentName="None";
+        studentSurname="None";
+        studentID="None";
+    }
+    Student(string name,string surname){
+        cout<<"2 parameter constructor worked."<<endl;
+        studentName=name;
+        studentSurname=surname;
+        studentID="None";
+    }
+    Student(string name,string surname,string ID){
+        cout<<"3 parameter constructor worked."<<endl;
+        studentName=name;
+        studentSurname=surname;
+        studentID=ID;
+    }
+    ~Student();//Parametre almaz
+    Student(const Student &oth);
+    void displayInformation();//üye fonksiyonlar(member function)
+    void setName(string name){
+        studentName=name;
+    };
+    void setSurname(string surname){
+        studentSurname=surname;
+    };
+    void setID(string ID){
+        studentID=ID;
+    };
+    string getName(){
+        return studentName;
+    };
+    string getSurname(){
+        return studentSurname;
+    };
+    string getID(){
+        return studentID;
+    };
+    
+};
+void Student::displayInformation(){
+    cout<<"Name:"<<studentName<<" "<<studentSurname<<endl;
+    cout<<"ID:"<<studentID<<endl;
+}
+Student::~Student(){
+    cout<<"Destructor worked."<<endl;
+}
+Student::Student(const Student &oth){
+    cout<<"Copy constructor worked."<<endl;
+    studentName=oth.studentName;
+    studentSurname=oth.studentSurname;
+    studentID=oth.studentID;
+}
